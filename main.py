429485b65ec72969501f6eb61f2781fed7fa5024
gomader <gh970818@hotmail.com>
2020-02-13 16:09:51 +0800

    test

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ pygame.init()
 screen = pygame.display.set_mode((600,500))
 myfont = pygame.font.Font(None,60)
 textImage = myfont.render("Hello",True,white)
+pygame.display.set_caption("Amazing Airport")
 while True:
     for event in pygame.event.get():
         if event.type in (QUIT,KEYDOWN):
             sys.exit()
     screen.fill(blue)
-    screen.blit(textImage,(100,100))
+    screen.blit(textImage,(300,250))
     pygame.display.update()
